40da77d92b39a1c383fecb272e15b8d5f1906996
MorningEatDinner <zhengxiaorui1998@gmail.com>
2024-07-28 19:43:47 +0800

    first commit

---
+++ apps/user/rpc/user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/xiaorui/ray_chat_2/apps/user/rpc/internal/config"
+	"github.com/xiaorui/ray_chat_2/apps/user/rpc/internal/server"
+	"github.com/xiaorui/ray_chat_2/apps/user/rpc/internal/svc"
+	"github.com/xiaorui/ray_chat_2/apps/user/rpc/user"
+)
+
+var configFile = flag.String("f", "etc/dev/user.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		user.RegisterUserServer(grpcServer, server.NewUserServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
